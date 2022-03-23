9ff56593aa17aeb5cebbe928282eee19193c663e
liyicky <jason@liyicky.com>
2022-03-23 10:17:02 +0900

    Initial Commit

---
+++ Mountain/MountainApp.swift
@@ -0,0 +1,17 @@
+//
+//  MountainApp.swift
+//  Mountain
+//
+//  Created by Jason Cheladyn on 2022/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MountainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
