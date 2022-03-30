cfaed1202de4d920e78e1370bf70b676af1f08c1
liyicky <jason@liyicky.com>
2022-03-30 15:21:27 +0900

    getting auth tokens

--- Mountain/ContentView.swift
+++ Mountain/ContentView.swift
@@ -6,12 +6,57 @@
 //
 
 import SwiftUI
+import Foundation
 
 struct ContentView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
+    let nm = NetworkManager()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack (alignment: .center, spacing: 20){
+            HStack (alignment: .center, spacing: 20){
+                Text("Email")
+                TextField("Name's placeholder", text: $email)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+            }
+            HStack (alignment: .center, spacing: 20){
+                Text("Password")
+                TextField("Name's placeholder", text: $password)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+            }
+            
+            HStack {
+                Button(action: {
+                    
+                    nm.registerUser(params: ["email": email, "password": password, "password_confirmation": password])
+                }, label: {
+                    Image(systemName: "clock")
+                    Text("Register")
+                })
+                .foregroundColor(Color.white)
+                .padding()
+                .background(Color.blue)
+                .cornerRadius(5)
+                
+                Button(action: {
+                    
+                    nm.loginUser(params: ["email": email, "password": password, "password_confirmation": password])
+                }, label: {
+                    Image(systemName: "clock")
+                    Text("Sign In")
+                })
+                .foregroundColor(Color.white)
+                .padding()
+                .background(Color.blue)
+                .cornerRadius(5)
+            }
+        }
     }
+        
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ Mountain/Views/SignUp.swift
@@ -0,0 +1,20 @@
+//
+//  SignUp.swift
+//  Mountain
+//
+//  Created by Jason Cheladyn on 2022/03/23.
+//
+
+import SwiftUI
+
+struct SignUp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignUp_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUp()
+    }
+}
